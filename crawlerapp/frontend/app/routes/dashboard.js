a2bf92ec2c1edcd53bfafade815a3d9bbe2a5f82
ADT\ahudson <ahudson@adt.com>
2019-05-01 16:18:27 -0500

    Added local storage storing of search requests

--- crawlerapp/frontend/app/routes/dashboard.js
+++ crawlerapp/frontend/app/routes/dashboard.js
@@ -2,12 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return {
-            id: 1,
-            url: 'google.com',
-            searchMethod: 'dfs',
-            depth: 15,
-            keyword: 'testing'
-        }
+        return this.store.findAll('search-request')
     }
 });
