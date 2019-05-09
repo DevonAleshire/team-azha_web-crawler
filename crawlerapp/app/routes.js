4ee8b2c5b213dfc4c839a5cadc1b58a674a76cec
AustinHudson <hudsaust@oregonstate.edu>
2019-05-08 20:58:16 -0500

    Rest of front end routing and mock api

--- crawlerapp/app/routes.js
+++ crawlerapp/app/routes.js
@@ -1,7 +1,28 @@
-// app/routes.js
 
-module.exports = function (app) {
-    app.get('*', function (req, res) {
-        res.sendFile(__dirname, './public/index.html'); // load our public/index.html file
+// app/routes.js
+// import searchHelper from './pageparse';
+
+module.exports = function (router) {
+    //Commented out for testing api route
+    // app.get('*', function (req, res) {
+    //     res.sendFile(__dirname, './public/index.html'); // load our public/index.html file
+    // });
+
+    router.route('/api/search').get(function (req, res) {
+        console.log('Hello from the node server');  
+
+        res.json({ 
+            response: "Response from server", 
+            isValid: true, 
+            url: req.query.url,
+            searchMethod: req.query.searchMethod,
+            depth: req.query.depth,
+            keyword: req.query.keyword  
+        });
     });
+
+    router.route('*').get(function (req, res) {
+        res.sendfile('./public/index.html'); // load our public/index.html file
+    });
+
 };
--- crawlerapp/frontend/app/routes/dashboard/search.js
+++ crawlerapp/frontend/app/routes/dashboard/search.js
@@ -1,15 +1,32 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
     model(params){
-        const formParams = this.store.findRecord('search-request', params.id)
+        const formParams = this.store.findRecord('search-request', params.id);
 
-        formParams.then(record => record.data)
+          //make call to backend here
+          const makeApiCall = (formData) => {
+            console.log('formdata apicall', formData);
+
+            const apiUrl = '/api/search/?' +
+                        `url=${formData.url}` +
+                        `&searchMethod=${formData.searchMethod}` +
+                        `&depth=${formData.depth}` +
+                        `&keyword=${formData.keyword}`;
+
+             return fetch(apiUrl)
+                .then(res => res.json())
+                .then(data => data); 
+        };
+
+        const returnData = formParams.then(record => record.data)
                   .then((data) => makeApiCall(data)); 
 
-        const makeApiCall = (formData) => {
-            console.log('formdata apicall', formData);
-        };
+        return RSVP.hash({
+            data: returnData,
+        });
+
     }
 
 
