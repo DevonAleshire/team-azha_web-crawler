06153485190bdd236bd43290cac45ae9d57da387
Devon Aleshire <devoale@gmail.com>
2019-05-12 09:36:12 -0700

    Initial tests and setup

---
+++ crawlerapp/app/tests/pageparse.test.js
@@ -0,0 +1,5 @@
+const p = require('../pageparse.js');
+
+test('Test Run', () => {
+  expect(true).toBe(true);
+});
--- crawlerapp/frontend/tests/unit/models/search-request-test.js
+++ crawlerapp/frontend/tests/unit/models/search-request-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { run } from '@ember/runloop'
 
 module('Unit | Model | search request', function(hooks) {
   setupTest(hooks);
@@ -9,5 +10,25 @@ module('Unit | Model | search request', function(hooks) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('search-request', {});
     assert.ok(model);
+  });
+
+  test('Search-request record created accurately', function (assert) {
+    //Lookup Store
+    let store = this.owner.lookup('service:store');
+    
+    //Create Record
+    const searchRequest = run(() => store.createRecord('search-request', {
+          url: 'url',
+          searchMethod: 'bfs',
+          depth: '0',
+          keyword: 'key'
+      }));
+    
+    //Verify Created Model Properties
+    assert.ok(searchRequest);
+    assert.equal(searchRequest.get('url'), 'url');
+    assert.equal(searchRequest.get('searchMethod'), 'bfs');
+    assert.equal(searchRequest.get('depth'), '0');
+    assert.equal(searchRequest.get('keyword'), 'key');
   });
 });
