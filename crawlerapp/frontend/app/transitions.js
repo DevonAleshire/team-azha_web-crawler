69e5510a139f37b8a554e8e7f0e00d242b5c12cc
AustinHudson <hudsaust@oregonstate.edu>
2019-05-29 23:38:07 -0500

    added transitions between routes

---
+++ crawlerapp/frontend/app/transitions.js
@@ -0,0 +1,13 @@
+export default function(){
+    this.transition(
+      this.fromRoute('dashboard'),
+      this.toRoute('loading'),
+      this.use('toLeft'),
+    );
+
+    this.transition(
+      this.fromRoute('search'),
+      this.toRoute('dashboard'),
+      this.use('toRight')
+    );
+}
