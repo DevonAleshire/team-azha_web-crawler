e299d15b7c1183a82376778cf0ab9920ada02404
Devon Aleshire <devoale@gmail.com>
2019-06-09 12:00:18 -0700

    updated for undefined url

--- crawlerapp/app/pageparse.js
+++ crawlerapp/app/pageparse.js
@@ -326,7 +326,7 @@ async function crawlBreadthFirst(url, searchDepth, currentDepth, keyword) {
                     depthQueue.push(navObj.linkObj.depth + 1);
                 }
 
-                if ((navObj.linkObj.depth + 1 <= searchDepth) && !nodeList.has(nextUrl)) {
+                if (nextUrl && (navObj.linkObj.depth + 1 <= searchDepth) && !nodeList.has(nextUrl)) {
                     nodeList.set(nextUrl, navObj.linkObj.depth + 1);
                     data.links.push({ source: navObj.linkObj.url, target: nextUrl });
                     console.log(`Adding new link ->\nSource: ${navObj.linkObj.url}\nTarget: ${nextUrl}\n`)
